578e42f3f9629ec5c2d376e8aa0a2674a355f614
kirk <aba636838@gmail.com>
2025-11-10 23:10:51 +0300

    fix

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
-import "../Timer/Timer.css"
+import "./Timer.css"
 
 const Timer = ({ onTimerComplete }) => {
   const [count, setCount] = useState(3);
