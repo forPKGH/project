ad30c35fbef641bb5ace6edcc3da374d00f70671
kirk <aba636838@gmail.com>
2025-11-11 22:54:44 +0300

    new manifest

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   base: './',
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: { enabled: true },
-      manifest: {
-        name: 'My PWA',
-        short_name: 'PWA',
-        start_url: './',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#42b883',
-        scope: './',
-        icons: [
-          { src: './pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: './pwa-512x512.png', sizes: '512x512', type: 'image/png' }
-        ]
-      }
-    })
-  ]
+  ],
 })
